56c47b3d2400d6b48e5156626499e2620a84e1a4
DUONG TIEN HIEU <tienhieuD@users.noreply.github.com>
2020-07-03 16:20:51 +0700

    INIT

---
+++ leads/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+from django.db import models
+
+class Lead(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.CharField(max_length=300)
+    created_at = models.DateTimeField(auto_now_add=True)
